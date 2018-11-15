b0fd9fedd65a7d45dae79e588d56fb5da51b0df3
Arturs Vilkajs <arturs.vilkajs@wunder.io>
2018-11-15 21:19:18 +0200

    Strip out some stuff we don't need from build.sh

--- build.sh
+++ build.sh
@@ -16,11 +16,6 @@ function parse_yaml {
       }
    }'
 }
-
-# Remember to update this on each release
-# Also update the changelog!
-VERSION=8
-
 pushd `dirname $0` > /dev/null
 ROOT=`pwd -P`
 popd > /dev/null
@@ -29,12 +24,6 @@ PROJECTCONF=$ROOT/project.yml
 eval $(parse_yaml $PROJECTCONF)
 ALIASFILE=${project_name}.aliases.drushrc.php
 ALIASTARGET=$HOME/.drush/$ALIASFILE
-
-if [ -z "$drush_alias_path" ]; then
-  ALIASPATH=$ROOT/drupal/conf/$ALIASFILE
-else
-  ALIASPATH=$ROOT/${drush_alias_path}/$ALIASFILE
-fi
 
 if [ -z "$wundertools_branch" ]; then
   GITBRANCH="master"
@@ -49,9 +38,6 @@ else
 fi
 
 
-if [ "$CI" = true ]; then
-  echo "You're using CI=$CI. This means that automatic updates for this script are skipped..."
-else
   VERSIONFILE=$ROOT/VERSION
   CHANGELOG=$ROOT/CHANGELOG
   CHANGELOGURL="https://raw.githubusercontent.com/$WUNDERTOOLSREPOSITORY/$GITBRANCH/CHANGELOG"
@@ -88,91 +74,45 @@ else
   MD5COMMAND="md5 -r"
 fi
 
-if [[ $1 == "reset" ]]; then
-  read -p "This will reset everything! Are you sure (y/n)? " -n 1 -r
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    cd $ROOT
-    vagrant destroy
-    rm -r $ROOT/ansible
-    rm $ALIASTARGET
-  fi
 # Only run when running vagrant up or provision
-elif [[ $1 == "up" || $1 == "provision" ]]; then
-  # First we check if there is update for this script
-  SELF=$(basename $0)
-  UPDATEURL="https://raw.githubusercontent.com/$WUNDERTOOLSREPOSITORY/$GITBRANCH/build.sh"
-  MD5SELF=$($MD5COMMAND $0 | awk '{print $1}')
-  MD5LATEST=$(curl -s $UPDATEURL | $MD5COMMAND | awk '{print $1}')
-  if [[ "$MD5SELF" != "$MD5LATEST" ]]; then
+# First we check if there is update for this script
+SELF=$(basename $0)
+UPDATEURL="https://raw.githubusercontent.com/$WUNDERTOOLSREPOSITORY/$GITBRANCH/build.sh"
+MD5SELF=$($MD5COMMAND $0 | awk '{print $1}')
+MD5LATEST=$(curl -s $UPDATEURL | $MD5COMMAND | awk '{print $1}')
+if [[ "$MD5SELF" != "$MD5LATEST" ]]; then
     while read -p "There is update for this script available. Update now ([y]es / [n]o / show [c]hangelog)?" -n 1 -r && [[ $REPLY =~ ^[Cc]$ ]]; do
-      curl -s $CHANGELOGURL | sed -e '/^'$CURRENT_VERSION'$/,$d'
+        curl -s $CHANGELOGURL | sed -e '/^'$CURRENT_VERSION'$/,$d'
     done
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-      cd $ROOT
-      curl -s -o $SELF $UPDATEURL
-      curl -s -o $CHANGELOG $CHANGELOGURL
-      echo "Update complete, please rerun any command you were running previously."
-      echo "See CHANGELOG for more info."
-      echo "Also remember to add updated script to git."
-      exit
+        cd $ROOT
+        curl -s -o $SELF $UPDATEURL
+        curl -s -o $CHANGELOG $CHANGELOGURL
+        echo "Update complete, please rerun any command you were running previously."
+        echo "See CHANGELOG for more info."
+        echo "Also remember to add updated script to git."
+        exit
     fi
-  fi
-  # Clone and update virtual environment configurations
-  if [ ! -d "$ROOT/ansible" ]; then
+fi
+# Clone and update virtual environment configurations
+if [ ! -d "$ROOT/ansible" ]; then
     git clone  -b $ansible_branch $ansible_remote $ROOT/ansible
     if [ -n "$ansible_revision" ]; then
-      cd $ROOT/ansible
-      git reset --hard $ansible_revision
-      cd $ROOT
+        cd $ROOT/ansible
+        git reset --hard $ansible_revision
+        cd $ROOT
     fi
-  else
+else
     if [ -z "$ansible_revision" ]; then
-      cd $ROOT/ansible
-      git pull
-      git checkout $ansible_branch
-      cd $ROOT
+        cd $ROOT/ansible
+        git pull
+        git checkout $ansible_branch
+        cd $ROOT
     else
-      cd $ROOT/ansible
-      git pull
-      git reset --hard $ansible_revision
-      cd $ROOT
+        cd $ROOT/ansible
+        git pull
+        git reset --hard $ansible_revision
+        cd $ROOT
     fi
-  fi
-
-  # If it is enabled in project.yml - get & update drupal/build.sh
-  if $buildsh_enabled; then
-    if [ -z "$buildsh_repository" ]; then
-      BUILDSHREPOSITORY="wunderio/build.sh"
-    else
-      BUILDSHREPOSITORY=$buildsh_repository
-    fi
-    if [ -n "$buildsh_revision" ]; then
-      curl -s -o $ROOT/drupal/build.sh https://raw.githubusercontent.com/$BUILDSHREPOSITORY/$buildsh_revision/build.sh
-    else
-      curl -s -o $ROOT/drupal/build.sh https://raw.githubusercontent.com/$BUILDSHREPOSITORY/$buildsh_branch/build.sh
-    fi
-  fi
-
-  # Ensure drush aliases file is linked
-  if [ ! -h $ALIASTARGET ] || [ ! "$(readlink $ALIASTARGET)" -ef "$ALIASPATH" ]; then
-    rm $ALIASTARGET
-    ln -s $ALIASPATH $ALIASTARGET
-  fi
-
-  if [ ! -z $externaldrupal_remote ]; then
-    if [ ! -z $externaldrupal_location ]; then
-      DRUPALLOCATION=$externaldrupal_location
-    else
-      DRUPALLOCATION="drupal/current"
-    fi
-    if [ ! -d $DRUPALLOCATION ]; then
-      mkdir -p $ROOT/$DRUPALLOCATION
-      if [ -z $externaldrupal_branch ]; then
-        $externaldrupal_branch = 'master'
-      fi
-      git clone -b $externaldrupal_branch $externaldrupal_remote $ROOT/$DRUPALLOCATION
-    fi
-  fi
 fi
